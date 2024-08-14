0969e4f4e2c7a33134f341f5dca9ac09e6b0132e
fabio7621 <d6200121a@gmail.com>
2024-08-14 12:13:54 +0800

    vueClass01

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vueClass01/',
   plugins: [vue()],
   resolve: {
     alias: {
